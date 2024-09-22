212d480ca7a28d8a56a93c8e90ed4cc7eb4ee883
nueizkk <nueizkk@gmail.com>
2024-09-22 17:28:47 +0900

    API_URL from page -> constants

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,13 +1,11 @@
 import { Metadata } from 'next';
-import Link from 'next/link';
+import { API_URL } from '../constants';
 import Movie from '../../components/movie';
 import styles from '../../styles/home.module.css';
 
 export const metadata: Metadata = {
   title: 'Home',
 };
-
-export const API_URL = `https://nomad-movies.nomadcoders.workers.dev/movies`;
 
 async function getMovies() {
   const response = await fetch(API_URL);
